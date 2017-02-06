b8176f16f8fd2d0b20f4c9cce12ea20044799513
Nicole Sanders <nsanders9022@gmail.com>
2017-02-06 10:13:49 -0800

    add a constructor to the program and creates instances of new Car objects using that way

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -12,6 +12,13 @@ namespace CarDealership
         public int Price;
         public int Miles;
 
+        public Car(string carMakeModel, int carPrice, int carMiles)
+        {
+            MakeModel = carMakeModel;
+            Price = carPrice;
+            Miles = carMiles;
+        }
+
         public bool WorthBuying(int maxPrice, int maxMileage)
         {
             return (Price < maxPrice && Miles < maxMileage);
@@ -22,25 +29,10 @@ namespace CarDealership
     {
         public static void Main()
         {
-            Car porsche = new Car();
-            porsche.MakeModel = "2014 Porsche 911";
-            porsche.Price = 114991;
-            porsche.Miles = 7864;
-
-            Car ford = new Car();
-            ford.MakeModel = "2011 Ford F450";
-            ford.Price = 55995;
-            ford.Miles = 14241;
-
-            Car lexus = new Car();
-            lexus.MakeModel = "2013 Lexus RX 350";
-            lexus.Price = 44700;
-            lexus.Miles = 20000;
-
-            Car mercedes = new Car();
-            mercedes.MakeModel = "Mercedes Benz CLS550";
-            mercedes.Price = 39900;
-            mercedes.Miles = 37979;
+            Car porsche = new Car("2014 Porsche 911", 114991, 7864);
+            Car ford = new Car("2011 Ford F450", 55995, 14241);
+            Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+            Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
             List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
 
