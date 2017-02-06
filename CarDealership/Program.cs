fdde3c81bca58ce0a530a8ffcd434513ede661f3
Nicole Sanders <nsanders9022@gmail.com>
2017-02-06 09:31:43 -0800

    add a car class and then 4 instances of the car class. All four objects are added into a list

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace CarDealership
 {
-    class Program
+    class Car
     {
-        static void Main(string[] args)
+        public string MakeModel;
+        public int Price;
+        public int Miles;
+    }
+
+    public class Program
+    {
+        public static void Main()
         {
+            Car porsche = new Car();
+            porsche.MakeModel = "2014 Porsche 911";
+            porsche.Price = 114991;
+            porsche.Miles = 7864;
+
+            Car ford = new Car();
+            ford.MakeModel = "2011 Ford F450";
+            ford.Price = 55995;
+            ford.Miles = 14241;
+
+            Car lexus = new Car();
+            lexus.MakeModel = "2013 Lexus RX 350";
+            lexus.Price = 44700;
+            lexus.Miles = 20000;
+
+            Car mercedes = new Car();
+            mercedes.MakeModel = "Mercedes Benz CLS550";
+            mercedes.Price = 39900;
+            mrecedes.Miles = 37979;
+
+            List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
         }
     }
+
+
 }
